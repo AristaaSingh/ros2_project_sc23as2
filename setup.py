6cec0785a7922f175001ed69c23ecca92c2ab4ad
AristaaSingh <sc23as2@leeds.ac.uk>
2026-05-05 23:24:07 +0100

    lab5 copied and combined into one python script

--- setup.py
+++ setup.py
@@ -20,10 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'first_step = ros2_project_sc23as2.first_step:main',
-            'second_step = ros2_project_sc23as2.second_step:main',
-            'third_step = ros2_project_sc23as2.third_step:main',
-            'fourth_step = ros2_project_sc23as2.fourth_step:main',
+            'project = ros2_project_sc23as2.project:main',
         ],
     },
 )
